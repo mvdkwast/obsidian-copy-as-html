3c22f7eecb88f74c664a5d80501e60474b4fd319
Martijn van der Kwast <martijn@vdkwast.com>
2022-10-30 00:48:19 +0200

    poc: copy page as html, embed images in url as data

--- main.ts
+++ main.ts
@@ -10,6 +10,73 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
+async function imageToUri(url: string): Promise<string> {
+	if (url.startsWith('data:')) {
+		return url;
+	}
+
+	const canvas = document.createElement('canvas');
+	const ctx = canvas.getContext('2d');
+
+	const base_image = new Image();
+	base_image.src = url;
+
+	console.log(`converting image: ${url.substring(0, 40)}`);
+
+	return new Promise((resolve, reject) => {
+		base_image.onload = () => {
+			canvas.width = base_image.width;
+			canvas.height = base_image.height;
+
+			ctx!.drawImage(base_image, 0, 0);
+
+			try {
+				const uri = canvas.toDataURL('image/png');
+				console.log(`converting done: ${uri.substring(0, 40)} (${canvas.height}x${canvas.width}`);
+				resolve(uri);
+			}
+			catch (err) {
+				console.log(`feiled ${url}`, err);
+				// leave original url
+				resolve(url);
+				// reject(err);
+			}
+
+			canvas.remove();
+		}
+	})
+}
+
+function cloneElement(element: HTMLElement): HTMLElement {
+	// @ts-ignore
+	return element.cloneNode(true);
+}
+
+async function replaceImageSource(image: HTMLImageElement): Promise<void> {
+	image.src = await imageToUri(image.src);
+}
+
+async function embedImages(element: HTMLElement): Promise<HTMLElement> {
+	const node = cloneElement(element);
+
+	const promises: Promise<void>[] = [];
+
+	console.log('element', element);
+
+	node.querySelectorAll('img')
+		.forEach(img => {
+			if (img.src && !img.src.startsWith('data:')) {
+				promises.push(replaceImageSource(img));
+			}
+		});
+
+	console.log(`awaiting ${promises.length} promises`)
+	await Promise.all(promises);
+
+	console.log('done');
+	return node;
+}
+
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
@@ -24,58 +91,32 @@ export default class MyPlugin extends Plugin {
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'sample-editor-command',
 			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				// console.log(editor.content);
+				// console.log(view.contentEl);
+				// editor.replaceSelection('Sample Editor Command');
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				const copy = await embedImages(view.contentEl);
+				console.log('copy', copy.outerHTML);
+
+				const data =
+					new ClipboardItem({
+						"text/html": new Blob([copy.outerHTML], {
+							type: "text/html"
+						}),
+						"text/plain": new Blob([copy.outerHTML], {
+							type: "text/plain"
+						}),
+					});
+
+				await navigator.clipboard.write([data]);
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
